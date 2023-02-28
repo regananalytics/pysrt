ee602b503588c001b380a3a7d77a6ce4e25a6d21
Dan <dan.regan@capellaspace.com>
2023-02-28 09:45:57 -0700

    start formalizing widget and overlay

--- pysrt/examples/re2_overlay.py
+++ pysrt/examples/re2_overlay.py
@@ -1,80 +1,74 @@
-from functools import wraps
-import keyboard
-import pygame
-import sys
-import time
+from pysrt.gui.widget import Widget, gauge_plot
 
-from pysrt.gui.widget import Widget
-from pysrt.gui.window import Window
-from pysrt.gui.overlay import Overlay
-from pysrt.gui.grid import draw_grid
+class RE2ROverlay:
 
-# fuschsia will serve as our colorkey for transparency
-fuchsia = (255, 0, 128) 
+    c_fg = (251, 255, 254)
+    c_bg = (0, 0, 0, 0)
+    c_fine = (39, 145, 92)
+    c_caution = (234, 160, 31)
+    c_danger = (174, 12, 9)
+    c_poison = (91, 15, 149)
 
-def time_draw(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        start = time.time()
-        result = func(*args, **kwargs)
-        print(f'Draw took {time.time() - start} seconds')
-        return result
-    return wrapper
+    pad = 10
+    height = 180
+    l_width = 285
+    r_width = 485
 
 
-def init_widget(w, h, color=fuchsia):
-    widget = pygame.Surface((w, h))
-    widget.fill(color)
-    return widget
+    def __init__(self):
+        self.last_data = {}
+        self.stats_widget = Widget(
+            self.l_width + self.r_width + self.pad, self.height, 
+            bg_color=self.c_bg, fg_color=self.c_fg
+        )
+        self.da_widget = Widget(
+            self.l_width, self.height, 
+            bg_color=self.c_bg, fg_color=self.c_fg
+        )
+        self.igt_widget = Widget(
+            self.r_width, self.height/3, 
+            bg_color=self.c_bg, fg_color=self.c_fg
+        )
+        self.hp_widget = Widget(
+            self.r_width, self.height/3*2, 
+            bg_color=self.c_bg, fg_color=self.c_fg
+        )
 
 
-def draw_bar_plot(surface, x, y, w, h, val, max, 
-    bar_color=(255, 0, 0), line_color=(255, 255, 255), bg_color=fuchsia
-):
-    # Draw a bar plot
-    # Bar
-    pygame.draw.rect(surface, bar_color, (x, y, 100, 20))
-    # Outline
-    pygame.draw.line(surface, line_color, (x, y+h), (x+w, y+h))
-    pygame.draw.line(surface, line_color, (x, y), (x, y+h))
-    pygame.draw.line(surface, line_color, (x+w, y), (x+w, y+h))
-    pygame.draw.line(surface, line_color, (x, y), (x+w, y))
-    
+    def update(self, data):
+        # Calculate stats
+        da = (data['da'] - 4000)/4000
 
-@time_draw
-def draw_func(overlay):
-    draw_bar_plot(overlay.surf, 100, 50, 150, 20, 650, 1000)
+        # draw update
+        self.stats_widget.init_surf()
 
+        # da widget
+        self.da_widget.init_surf()
+        da_gauge = gauge_plot(da, self.l_width, bar_color=self._da_color(da))
+        write_text(self.da_widget.surface, 'DA', 0, 15, size=20)
+        
+        
 
-def main(title:str, draw_func):
-    # Use the window title to create a window object
-    win = Window.from_title(title, partial=True)
-    # Create an overlay object for the window
-    overlay = Overlay(win)
-    # Set the transparency color to fuchsia
-    overlay.set_transparency(colorkey=fuchsia)
+    def _hp_color(self, hp, poisoned=False):
+        if poisoned:
+            return self.c_poison
+        if hp > 800:
+            return self.c_fine
+        elif hp > 360:
+            return self.c_caution
+        else:
+            return self.c_danger
+        
 
-    # Create widget
-    overlay.set_draw_func(draw_func)
-
-    # Blit to overlay
-    print('Press "q" to quit...')
-
-    # Finally, write the main loop to update the overlay each frame
-    # until the user presses the 'q' key
-    while True:
-        start = time.time()
-        overlay.update()
-        if keyboard.is_pressed('q'):
-            overlay.quit()
-            break
-        time.sleep(max(0, 1/10 - (time.time() - start)))
+    def _da_color(self, da):
+        da_rank = self._da_rank(da)
+        if da_rank > 7:
+            return self.c_danger
+        elif da_rank > 5:
+            return self.c_caution
+        else:
+            return self.c_fine
 
 
-if __name__ == '__main__':
-    args = sys.argv[1:]
-    if not args:
-        title = 'Visual Studio Code'
-    else:
-        title = args[0]
-    main(title, draw_func)
+    def _da_rank(self, da):
+        return int(da/1000)
--- pysrt/gui/widget.py
+++ pysrt/gui/widget.py
@@ -2,6 +2,13 @@ from math import pi, tan
 import pygame
 import pygame.gfxdraw
 
+GRID = False
+
+pad = 10
+height = 200
+da_width = 285
+hp_width = 485
+
 fg_color = (251, 255, 254)
 bg_color = (0, 0, 0)
 
@@ -10,8 +17,35 @@ caution = (234, 160, 31)
 danger = (174, 12, 9)
 poison = (91, 15, 149)
 
+
+class Widget:
+
+    def __init__(self, width, height, pad=10, bg_color=(0, 0, 0), fg_color=(255, 255, 255)):
+        self.width = width
+        self.height = height
+        self.pad = pad
+        self.bg_color = bg_color
+        self.fg_color = fg_color
+
+    
+    def init_surf(self):
+        self.surface = pygame.Surface((self.width, self.height))
+        self.surface.fill(self.bg_color)
+        return self.surface
+
+
+    def blit(self, surface, x, y):
+        surface.blit(self.surface, (x, y))
+        return surface
+
+
+    
+
+
+
+
 def write_text(surface, text, x, y, font='arial', size=12, justify='left', color=fg_color, bold=False):
-    font = pygame.font.SysFont(font, size, bold)
+    font = pygame.font.SysFont(font, int(round(size)), bold)
     text = font.render(text, True, color)
     if justify == 'center':
         x -= text.get_width() // 2
@@ -80,7 +114,7 @@ def gauge_plot(val, width=100,
     # Draw bar
     if val > 0:
         bar = pygame.Surface((width+1, width), pygame.SRCALPHA)
-        draw_aaarc(bar, r, r, r-thickness, bar_color, (0, 0, 0, 0), r*ratio-thickness-1)
+        draw_aaarc(bar, r, r, r-thickness, bar_color, (0, 0, 0, 0), r*ratio-thickness)
         bg_poly = lambda points: draw_aapolygon(bar, points, (0, 0, 0, 0))
         t = pi*(1-val/100)
         if val >= 100:
@@ -106,16 +140,18 @@ def gauge_plot(val, width=100,
 
 
 pygame.init()
-display = pygame.display.set_mode((1000, 200), pygame.NOFRAME)
+display = pygame.display.set_mode((da_width + hp_width + pad*3, height-pad*2), pygame.NOFRAME)
 display.fill(bg_color)
 
 def update(da, hp, hp_max, poisoned=False, ehp=None, ehp_max=None):
 
-    blank = pygame.Surface((1000, 200))
-    blank.fill(bg_color)
-    display.blit(blank, (0, 0))
+    _height = height-pad*2
+    _lg_font = _height/2.414
+    _md_font = _height/5
+    _sm_font = _height/5-pad
+    
 
-    # DA
+    # DA Calculations
     da_ticks = [7000, 6000, 5000]
     if da > 7000:
         da_color = danger
@@ -123,20 +159,9 @@ def update(da, hp, hp_max, poisoned=False, ehp=None, ehp_max=None):
         da_color = caution
     else:
         da_color = fine
+    da_val = (da-4000)/4000*100
 
-    display.blit(gauge_plot((da-4000)/4000*100, 300, bar_color=da_color), (20, 30))
-    write_text(display, 'DA', 20, 15, size=30)
-    write_text(display, f'{da}', 170, 150, size=30, justify='center')
-    write_text(display, f'{da/1000:.0f}', 170, 90, size=60, justify='center', color=caution, bold=True)
-
-    write_text(display, 'IGT', 410, 15, size=30, justify='right')
-    write_text(display, f'00:00:00', 535, 15, size=30, justify='right')
-    # write_text(display, '▲', 610, 22, size=20, justify='right', color=danger)
-    write_text(display, '▼', 610, 22, size=20, justify='right', color=fine)
-    write_text(display, f'00:00', 675, 15, size=30, justify='right')
-    write_text(display, f'00:00', 820, 15, size=30, justify='right', bold=False)
-
-    # HP
+    # HP Calculations
     hp_ticks = [800, 360]
     if poisoned:
         hp_color = poison
@@ -147,17 +172,59 @@ def update(da, hp, hp_max, poisoned=False, ehp=None, ehp_max=None):
     else:
         hp_color = danger
 
-    display.blit(bar_plot(76, w=400, h=40 if ehp else 90, bar_color=hp_color, bg_color=bg_color), (430, 75))
-    write_text(display, 'CHP', 410, 78 if ehp else 103, size=30, justify='right')
-    write_text(display, f'{hp} / {hp_max}', 820, 78 if ehp else 103, size=30, justify='right')
 
-    # EHP
-    if ehp:
-        display.blit(bar_plot(82, w=550, h=40, bar_color=danger, bg_color=bg_color), (430, 140))
-        write_text(display, 'EHP', 410, 143, size=30, justify='right')
-        write_text(display, f'{ehp} / {ehp_max}', 820, 143, size=30, justify='right')
+    # Background
+    bkgd = pygame.Surface((da_width + hp_width + pad*3, height-pad*2))
+    bkgd.fill(bg_color)
+    display.blit(bkgd, (0, 0))
+
+    # DA Region
+    da_region = pygame.Surface((da_width-2, _height-2))
+
+    da_region.blit(gauge_plot(da_val, da_width-2, bar_color=da_color), (0, _height-(da_width/2+pad)))
+    write_text(da_region, 'DA', 0, pad*1.5, size=_md_font)
+    write_text(da_region, f'{da}', da_width/2+1, _height-_sm_font-pad/2, size=_sm_font, justify='center')
+    write_text(da_region, f'{da/1000:.0f}', da_width/2+1, _height/2-pad*1.5, size=_lg_font, justify='center', color=caution, bold=True)
+
+    display.blit(da_region, (pad+1, 1))
+
+    if GRID:
+        box_outline(display, da_width, height-pad*3, pad, pad, fg_color, 1)
+        pygame.draw.line(display, fg_color, (da_width/2+pad, pad), (da_width/2+pad, height-pad*2), 1)
+        pygame.draw.line(display, fg_color, (pad, (height-pad)/2), (da_width+pad-1, (height-pad)/2), 1)
+
+    # HP Region
+    hp_left = da_width+pad*2
+    bar_width = hp_width/5*4 - pad
+    bar_height = _height/3-pad*2
+ 
+    hp_region = pygame.Surface((hp_width-2, _height-2))
+
+    write_text(hp_region, 'IGT', hp_width/5-pad*1.5, pad*1.5, size=_md_font, justify='right')
+    write_text(hp_region, f'00:00:00', hp_width/5+pad/2.5, pad*2, size=_md_font-pad/2, justify='left')
+    write_text(hp_region, f'00:00', hp_width-pad*1.5, pad*2, size=_md_font-pad/2, justify='right', bold=False)
+
+    # delta
+    # write_text(hp_region, '▲', hp_width/2+pad*4.5, pad*2.25, size=_sm_font, justify='right', color=danger)
+    write_text(hp_region, '▼', hp_width/2+pad*4.5, pad*2.25, size=_sm_font, justify='right', color=fine)
+    write_text(hp_region, f'00:00', hp_width/2+pad*5, pad*2.25, size=_sm_font, justify=' ', color=fine)
+
+    hp_region.blit(bar_plot(76, w=bar_width, h=bar_height, bar_color=hp_color, bg_color=bg_color), (hp_width/5, bar_height+pad*3))
+    write_text(hp_region, 'CHP', hp_width/5-pad, bar_height+pad*3.5, size=_md_font, justify='right')
+    write_text(hp_region, f'{hp} / {hp_max}', hp_width-pad*2, bar_height+pad*3.75+1, size=_sm_font, justify='right')
+
+    hp_region.blit(bar_plot(82, w=bar_width, h=bar_height, bar_color=danger, bg_color=bg_color), (hp_width/5, bar_height*2+pad*5))
+    write_text(hp_region, 'EHP', hp_width/5-pad, bar_height*2+pad*5.5, size=_md_font, justify='right')
+    write_text(hp_region, f'{ehp} / {ehp_max}', hp_width-pad*2, bar_height*2+pad*5.75+1, size=_sm_font, justify='right')
+
+    display.blit(hp_region, (hp_left+1, 1))
+
+    if GRID:
+        box_outline(display, hp_width, height-pad*3, pad*2+da_width, pad, fg_color, 1)
+        pygame.draw.line(display, fg_color, (hp_left+hp_width/2, pad), (hp_left+hp_width/2, height-pad*2), 1)
+        pygame.draw.line(display, fg_color, (hp_left, (height-pad)/2), (hp_left+hp_width+1, (height-pad)/2), 1)
 
 
 while True:
-    update(5230, 765, 1500, None, None)
+    update(5230, 765, 1200, False, 855, 3500)
     pygame.display.flip()
